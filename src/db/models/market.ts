dc7293f1cf83a78ff90ae95a9c9d5d405131d131
Rickos99 <Rickos99@users.noreply.github.com>
2022-01-12 14:07:26 +0100

    Updated models

---
+++ src/db/models/market.ts
@@ -0,0 +1,45 @@
+var mongoose = require("mongoose");
+
+export interface Market {
+    name: string;
+    demand: number;
+    supply: number;
+    basePrice: number;
+    price: {
+        validUntil: Date;
+        updatedAt: Date;
+        value: number;
+        currency: string;
+    };
+}
+
+var marketSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            unique: true,
+            index: true
+        },
+        demand: {
+            type: Number,
+            min: 0
+        },
+        supply: {
+            type: Number,
+            min: 0
+        },
+        basePrice: {
+            type: Number,
+            required: true
+        },
+        price: {
+            validUntil: Date,
+            updatedAt: Date,
+            value: Number,
+            currency: String
+        }
+    },
+    { versionKey: false }
+);
+
+export const market = mongoose.model("market", marketSchema);
